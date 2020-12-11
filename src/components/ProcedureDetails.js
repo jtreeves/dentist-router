8861fb77e18acc5bb0e6dd63b2e4c8b4e5e228a1
jtreeves <jackson.t.reeves@gmail.com>
2020-12-11 11:48:16 -0500

    [ProcedureDetails] Create new component for new page

---
+++ src/components/ProcedureDetails.js
@@ -0,0 +1,11 @@
+import Procedures from "./Procedures";
+
+function ProcedureDetails() {
+    return(
+        <>
+            <h1>Procedure Details</h1>
+        </>
+    )
+}
+
+export default ProcedureDetails
