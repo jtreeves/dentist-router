e54a0bbcf6d310e249d46a0d7bc64a91cfcc32a3
jtreeves <jackson.t.reeves@gmail.com>
2020-12-11 11:04:52 -0500

    [App] Import BrowserRouter and Route from react-router-dom and wrap BrowserRouter around main div in return statement

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import './App.css'
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import Home from './components/Home'
 import Contact from './components/Contact'
 import Procedures from './components/Procedures'
 
+import './App.css'
+
 function App() {
     return (
-        <div className='App-header'>
-            <Home />
-            <Contact />
-            <Procedures />
-        </div>
+        <BrowserRouter>
+            <div className='App-header'>
+                <Home />
+                <Contact />
+                <Procedures />
+            </div>
+        </BrowserRouter>
     )
 }
 
